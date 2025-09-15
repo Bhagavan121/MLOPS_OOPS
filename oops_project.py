bb979f88a5cea7058b5523a2cbc748bc7e18a188
Bhagavan <bhagavandheeravath0@gmail.com>
2025-09-15 22:34:45 +0530

    encapsulation ,getter and setter are covered

--- oops_project.py
+++ oops_project.py
@@ -1,17 +1,42 @@
 class chatbook:
+
+    __user_id = 1
+
     def __init__(self):
-        self.username = ""
-        self.password = ""
+        self.id = chatbook.__user_id
+        chatbook.__user_id += 1
+        self.__name = "Default User"
+        self.username = ''
+        self.password = ''
         self.loggedin = False
-        self.menu()
+        # self.menu()
+
+    @staticmethod
+    def get_id():
+        return chatbook.__user_id
+    
+    @staticmethod
+    def set_id(val):
+        chatbook.__user_id = val
+
+
+    def get_name(self):
+        return self.__name
+    
+
+    def set_name(self, value):
+        self.__name = value
+
 
     def menu(self):
-        user_input = input("""Welcome to Chatbook !! How would you like to proceed?
-    1. Press 1 to signup
-    2. Press 2 to signin
-    3. Press 3 to write a post
-    4. Press 4 to message a friend
-    5. Press any other key to exit""")
+        user_input = input(""""Welcome to Chatbook !! How would you like to proceed?
+                           1. Press 1 to signup
+                           2. Press 2 to signin
+                           3. Press 3 to write a post
+                           4. Press 4 to message a friend
+                           5. Press any other key to exit
+                           
+                           -> """)
         if user_input == "1":
             self.signup()
         elif user_input == "2":
@@ -23,6 +48,7 @@ class chatbook:
         else:
             exit()
 
+
     def signup(self):
         email = input("enter your email here -> ")
         pwd = input("setup your password here -> ")
@@ -33,12 +59,12 @@ class chatbook:
         self.menu()
 
     def signin(self):
-        if self.username == '' and self.password == '':
+        if self.username=='' and self.password=='':
             print("Please signup first by pressing 1 in the main menu")
         else:
             uname = input("enter your email/username here -> ")
-            pwd = input("Enter your password here -> ")
-            if self.username == uname and self.password == pwd:
+            pwd = input("ENter your password here -> ")
+            if self.username==uname and self.password==pwd:
                 print("You have signed in successfully !!")
                 self.loggedin = True
             else:
@@ -47,7 +73,7 @@ class chatbook:
         self.menu()
 
     def my_post(self):
-        if self.loggedin == True:
+        if self.loggedin==True:
             txt = input("Enter your message here -> ")
             print(f"Following content has been posted -> {txt}")
         else:
@@ -56,7 +82,7 @@ class chatbook:
         self.menu()
 
     def sendmsg(self):
-        if self.loggedin == True:
+        if self.loggedin==True:
             txt = input("Enter your message here -> ")
             frnd = input("Whom to send the msg? -> ")
             print(f"Your message has been sent to {frnd}")
@@ -65,5 +91,5 @@ class chatbook:
         print("\n")
         self.menu()
 
-obj = chatbook()
-1
+
+# user1 = chatbook()
